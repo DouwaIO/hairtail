c796da0d971dbecc81baf6281ee1b636431fa1e0
jwj831223 <woshijiangjinqi@163.com>
2019-04-18 10:41:37 +0800

    test

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # set -x
 
+
 echo "compile starting"
 APPPATH="/go/src/github.com/DouwaIO/hairtail"
 docker run --rm -v ${PWD}:${APPPATH} -w ${APPPATH} golang:1.9.7 sh -c "cd ${APPPATH}/src/cmd; go build -o ${APPPATH}/dist/htail"
