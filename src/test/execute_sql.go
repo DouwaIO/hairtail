4ea6c3236d6ac55813e56379ec3fac6b6cfa7cf1
jwj831223 <woshijiangjinqi@163.com>
2019-04-23 14:33:04 +0800

    sql

---
+++ src/test/execute_sql.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/Sirupsen/logrus"
+)
+
+func check(connect string, sql string) (error){
+	db,err := gorm.Open("postgres", connect)
+	if err != nil {
+		logrus.Errorln(err)
+		logrus.Fatalln("database ping attempts failed")
+	}
+	logrus.Infof("连接成功了")
+	if err != nil{
+		return err
+	}
+
+	_,err = db.Exec(sql)
+	if err != nil{
+		return err
+	}
+    
+}
+
+func main() {
+
+}
