e052eb8edd94498dd5802c859f935400985be4c0
jwj831223 <woshijiangjinqi@163.com>
2019-04-25 09:03:22 +0800

    数据展开

--- src/test/split.go
+++ src/test/split.go
@@ -20,22 +20,32 @@ func split(data map[string]interface{},output_dict map[string]interface{},key__ 
         if typeof(data[key]) == "map[string]interface {}"{
             _,list__ := split(data[key].(map[string]interface{}),output_dict,key__ + key + "__")
             output_list = append(output_list, list__...)
-            fmt.Println("out_",list__)
         }else if typeof(data[key]) != "[]interface {}"{
             output_dict[key__+key] = data[key]
         }else{
             for i:=0; i < len(data[key].([]interface{})); i++ {
                 dict__,_ := split(data[key].([]interface{})[i].(map[string]interface{}),make(map[string]interface{}),key__ + key + "__")
-                fmt.Println(dict__)
                 output_list = append(output_list, dict__)
-                fmt.Println(output_list)
             }
         }
-        
     }
-
-    fmt.Println("out",output_list)
     return output_dict,output_list
+}
+
+func split_data(data []byte)  ([]interface{}){
+    myMap:=make(map[string]interface{})
+    json.Unmarshal(data,&myMap)
+    output_dict := make(map[string]interface{})
+    dict_data,list_data := split(myMap,output_dict,"")
+
+    if len(list_data) == 0{
+        list_data = append(list_data,dict_data)
+    }else{
+        for i:=0; i < len(list_data); i++ {
+            for k ,v := range dict_data { list_data[i].(map[string]interface{})[k] = v }
+        } 
+    }
+    return list_data
 }
 
 
@@ -44,14 +54,20 @@ func split(data map[string]interface{},output_dict map[string]interface{},key__ 
 func main() {
 
     str := "{\"name\":\"fabric_stock_in\",\"type\":\"add\",\"key\":\"F000323546\",\"time\":\"2019-04-23T03:10:33.775Z\",\"data\":{\"bill_no\":\"string\",\"details\":[{\"grade\":\"string\",\"location_no\":\"string\",\"line\":\"ASDF111\"},{\"fabric_no\":\"string\"}],\"bill_type_name\":\"采购入库\"}}"
-    myMap:=make(map[string]interface{})
-    output_dict := make(map[string]interface{})
+    // myMap:=make(map[string]interface{})
+    // output_dict := make(map[string]interface{})
       
-    json.Unmarshal([]byte(str),&myMap)
+    // json.Unmarshal([]byte(str),&myMap)
 
-    aa,bb := split(myMap,output_dict,"")
-    fmt.Println(aa)
-    fmt.Println("test",bb)
-    fmt.Println(output_dict)
+    // aa,bb := split(myMap,output_dict,"")
+    // fmt.Println(aa)
+    // fmt.Println("test",bb)
+    // fmt.Println(output_dict)
 
+
+
+    bb := split_data([]byte(str))
+
+    // fmt.Print(aa)
+    fmt.Print(bb)
 }
