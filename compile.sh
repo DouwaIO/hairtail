af75651503d354775bc9d1ac176d2281e7eddcaa
hellwen <hellwen.wu@gmail.com>
2019-08-19 09:53:40 +0800

    remove compile

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# set -x
-
-
-echo "compile starting"
-APPPATH="/go/src/github.com/DouwaIO/hairtail"
-docker run --rm -v ${PWD}:${APPPATH} -w ${APPPATH} golang:1.9.7 sh -c "cd ${APPPATH}/src/cmd; go build -o ${APPPATH}/dist/htail"
-echo "compile ending"
-echo
